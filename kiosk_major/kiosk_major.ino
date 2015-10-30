ae7af89e8d4a288ffcfd3fe747258e1b42aa1a28
Amorn <benamorn@gmail.com>
2015-10-30 15:49:47 +0700

    organize code

--- kiosk_major/kiosk_major.ino
+++ kiosk_major/kiosk_major.ino
@@ -8,25 +8,17 @@
 
 
 byte mac[] = { 0x98, 0x5a, 0xeb, 0xdb, 0x5b, 0x8b }; // get from the Network Setting
-//byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-//byte ip[] = { 192, 168, 1, 177 }; // IP Address for the shield
-IPAddress ip(192 ,168, 1, 251);
-IPAddress dns1( 192,168,1,1 );
-IPAddress gateway( 192,168,1,1 );
-IPAddress subnet( 255,255,255,0 );
+//byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // or use any
 
-//byte ip[] = {192,168,1,251};
-//byte server[] = { 64, 233, 187, 99 }; // Google Server
+IPAddress ip(192 ,168, 1, 251); // IP Address for the shield, Should do fix IP
+
 byte server[] = { 192, 168, 1, 46 };
-//char server[] = "www.google.com";
-//char server[] = "http://192.168.1.46";
-//IPAddress server(64, 233, 187, 99);
+
 const int LED = 13;
 const int ALARM = 8;
 const int SENSOR = 53; // Declare sensor Pin position
 int value = 0;
 
-//Client client(server)
 EthernetClient client;
 
 void setup() {
@@ -51,8 +43,6 @@ void setup() {
 
 void loop() {
    // put your main code here, to run repeatedly:
-
-     //digitalWrite(ALARM,HIGH);
      value = digitalRead(SENSOR); 
      Serial.print(value);
      if(value == HIGH){ 
@@ -77,19 +67,4 @@ void loop() {
 //     Serial.print(c);
 //}
 //}
-// if (!client.connected()) {
-//    Serial.println("disconnecting.");
-//     digitalWrite(ALARM,LOW);
-//     Serial.println("reconnecting.");
-//     client.stop();
-//     if(client.connect(server,80)){
-//      Serial.println("Connected Succeed.");
-//      client.println("GET /kiosk/count.php?count=1 HTTP/1.1");
-//     client.println("Host: http://192.168.1.46");
-//     client.println("Connection: close");
-//     client.println();
-//     }
-//    
-   // while(true);
-// }
 }
